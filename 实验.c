cabd1a78f6e4b21d0512ddd1039341d1fb44a554
Administrator <Administrator@DESKTOP-1MU7V9S>
2022-01-21 15:28:10 +0800

    添加项目文件。

---
+++ 实验.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+//int temp(int a)
+//{
+//	int i = 0;
+//	while (a)
+//	{
+//		if (a % 2 == 1)
+//			i++;
+//		a = a / 2;
+//	}
+//	return i;
+//}
+int main()
+{
+	int a = 0;
+	scanf_s("%d", &a);
+	printf("%d", temp(a));
+	return 0;
+}
