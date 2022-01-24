a22b513b9fbab9149b964b999c3f6f75c4fa1141
yaoxigeigei <98148031+yaoxigeigei@users.noreply.github.com>
2022-01-24 18:08:00 +0800

    Add files via upload

---
+++ 指针数组.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int arr1[] = { 1,2,3,4,5,6 };
+	int arr2[] = { 2,3,4,5,6,7 };
+	int arr3[] = { 3,4,5,6,7,8 };
+	int* arr4[] = { arr1,arr2,arr3 };
+	int i = 0, j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			printf("%d", *(arr4[i] + j));
+		}
+		printf("\n");
+	}
+	return 0;
+}
