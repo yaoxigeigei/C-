2fa6be8e8c6fa5cc7e0a19f93446893f4da47c0c
yaoxigeigei <98148031+yaoxigeigei@users.noreply.github.com>
2022-01-26 11:02:14 +0800

    Add files via upload

---
+++ (二级指针)指针的用法(2).c
@@ -0,0 +1,24 @@
+#include<stdio.h>         //二级指针可以传 二级指针本身 一级指针地址 指针数组的变量名	
+//void test1(int** p)
+//{
+//
+//}
+//int main()
+//{
+//	int arr[3] = {0};
+//	int* p = arr;
+//	int** pp = &p;
+//	test1(&p);
+//	test1(pp);
+//	return 0;
+//}
+
+
+void test2(int** p)
+{}
+int main()
+{
+	int* arr[5];
+	test(arr);
+	return 0;
+}
