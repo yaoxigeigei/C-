300e5af0cb8d58ba9d9e734790646a7ad0ca2ec1
yaoxigeigei <98148031+yaoxigeigei@users.noreply.github.com>
2022-01-26 18:14:33 +0800

    Update 用函数和指针写计算器(7).c

--- 用函数和指针写计算器(7).c
+++ 用函数和指针写计算器(7).c
@@ -24,10 +24,10 @@ int divide(int a, int b)
 {
 	return a / b;
 }
-void C(int(*p)(int, int))  //»Øµ÷º¯Êı	
+void C(int(*p)(int, int))  //å›è°ƒå‡½æ•°	
 {
 	int a = 0, b = 0;
-	printf("ÊäÈëÁ½¸öÊı:");
+	printf("è¾“å…¥ä¸¤ä¸ªæ•°:");
 	scanf_s("%d%d", &a, &b);
 	printf("%d\n", p(a, b));
 }
@@ -39,7 +39,7 @@ int main()
 	{
 		menu();
 		again:
-		printf("ÇëÊäÈëÄãÒªµÄĞòÁĞÊı:");
+		printf("è¯·è¾“å…¥ä½ è¦çš„åºåˆ—æ•°:");
 		scanf_s("%d", &input);
 		switch (input)
 		{
@@ -56,9 +56,9 @@ int main()
 			C(divide);
 			break;
 		case 0:
-			printf("ÍË³öÓÎÏ·\n\n\n\n"); 
+			printf("é€€å‡ºæ¸¸æˆ\n\n\n\n"); 
 			break;
-		default:printf("ÊäÈëµÄĞòºÅ²»ºÏ·¨,ÇëÖØĞÂÊäÈë:\n"); 
+		default:printf("è¾“å…¥çš„åºå·ä¸åˆæ³•,è¯·é‡æ–°è¾“å…¥:\n"); 
 			goto again;
 			break;
 		}
@@ -71,58 +71,58 @@ int main()
 
 
 
-//½ø»¯¼ÆËãÆ÷
-//#include<stdio.h>
-//int Add(int a, int b)
-//{
-//	return a + b;
-//}
-//int subtract(int a, int b)
-//{
-//	return a - b;
-//}
-//int multiply(int a, int b)
-//{
-//	return a * b;
-//}
-//int divide(int a, int b)
-//{
-//	return a / b;
-//}
-//void menu()
-//{
-//	printf("***************************************\n");
-//	printf("******   1:Add       2:subtract  ******\n");
-//	printf("*********3:multiply  4:divide    ******\n");
-//	printf("***********     0:exit     ************\n");
-//	printf("***************************************\n");
-//	printf("***************************************\n");
-//}
-//int main()
-//{
-//	int input = 0;
-//	int a = 0, b = 0;
-//	do
-//	{	
-//		menu();
-//		printf("ÇëÊäÈëÄãÒªÑ¡ÔñµÄĞòÁĞÊı:");
-//		scanf_s("%d", &input);
-//		if (input == 0) 
-//		{
-//			printf("ÍË³öÓÎÏ·\n\n\n\n");
-//		}
-//		else if (input >= 1 && input <= 4)
-//		{
-//			printf("ÇëÊäÈëÁ½¸ö²Ù×÷Êı:");
-//			scanf_s("%d%d", &a, &b);
-//			//º¯ÊıÖ¸ÕëÊı×é----ÓÖ³Æ×ªÒÆ±í
-//			int (*p[5])(int a, int b) = { 0,Add,subtract,multiply,divide };
-//			printf("%d\n", p[input](a, b));
-//		}
-//		else
-//		{
-//			printf("ÊäÈëµÄĞòÁĞºÅ·Ç·¨,ÇëÖØĞÂÊäÈë");
-//		}
-//	} while (input);
-//	return 0;
-//}
+è¿›åŒ–è®¡ç®—å™¨
+#include<stdio.h>
+int Add(int a, int b)
+{
+	return a + b;
+}
+int subtract(int a, int b)
+{
+	return a - b;
+}
+int multiply(int a, int b)
+{
+	return a * b;
+}
+int divide(int a, int b)
+{
+	return a / b;
+}
+void menu()
+{
+	printf("***************************************\n");
+	printf("******   1:Add       2:subtract  ******\n");
+	printf("*********3:multiply  4:divide    ******\n");
+	printf("***********     0:exit     ************\n");
+	printf("***************************************\n");
+	printf("***************************************\n");
+}
+int main()
+{
+	int input = 0;
+	int a = 0, b = 0;
+	do
+	{	
+		menu();
+		printf("è¯·è¾“å…¥ä½ è¦é€‰æ‹©çš„åºåˆ—æ•°:");
+		scanf_s("%d", &input);
+		if (input == 0) 
+		{
+			printf("é€€å‡ºæ¸¸æˆ\n\n\n\n");
+		}
+		else if (input >= 1 && input <= 4)
+		{
+			printf("è¯·è¾“å…¥ä¸¤ä¸ªæ“ä½œæ•°:");
+			scanf_s("%d%d", &a, &b);
+			//å‡½æ•°æŒ‡é’ˆæ•°ç»„----åˆç§°è½¬ç§»è¡¨
+			int (*p[5])(int a, int b) = { 0,Add,subtract,multiply,divide };
+			printf("%d\n", p[input](a, b));
+		}
+		else
+		{
+			printf("è¾“å…¥çš„åºåˆ—å·éæ³•,è¯·é‡æ–°è¾“å…¥");
+		}
+	} while (input);
+	return 0;
+}
