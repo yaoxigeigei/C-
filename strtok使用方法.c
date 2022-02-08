edd48b7043cc132cd14853cbb698ee9ff5c4ff5f
yaoxigeigei <98148031+yaoxigeigei@users.noreply.github.com>
2022-02-08 20:18:57 +0800

    Add files via upload

---
+++ strtok使用方法.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+int main()
+{
+	char arr[] = "192.168.0.1";
+	char* a = ".";
+	char buf[200] = { 0 };
+	strcpy(buf, arr);
+	char* e=NULL;
+	//printf("%s", e);
+	for (e = strtok(buf, a); e != NULL; e = strtok(NULL, a))
+	{
+		printf("%s ", e);
+	}
+	return 0;
+}
